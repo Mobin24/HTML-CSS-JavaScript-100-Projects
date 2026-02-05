ea89ac95e280f8676ea188e6758e2e9453e2dcfb
Mobin24 <mdabdullahallmobin00@gmail.com>
2026-02-05 13:09:07 +0600

    new code

---
+++ InterChangeablecolor/app.js
@@ -0,0 +1,38 @@
+const btns=document.querySelectorAll(".btn");
+const body=document.body
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    number = btn.value;
+    changeBackground(number);
+  });
+});
+
+
+function changeBackground(number) {
+  // console.log(number);
+  body.className = "";
+
+  switch (number) {
+    case "purple":
+      body.classList.add("purple");
+      break;
+    case "blue":
+      body.classList.add("blue");
+      break;
+    case "red":
+      body.classList.add("red");
+      break;
+    case "green":
+      body.classList.add("green");
+      break;
+    case "yellow":
+      body.classList.add("yellow");
+      break;
+    case "teal":
+      body.classList.add("teal");
+      break;
+    default:
+      break;
+  }
+}
